c39ed9905084c4b74cb57183a40bb4fd5c4a6c06
Christian <Christian@10.31.41.245>
2017-02-08 09:46:02 -0500

    The changes done to the code are the following:
    Changes done to the title of the aplication's GUI, name was placed as
    title.
    
    Size of the aplication's screen was changed to a square (200x200).
    
    Start up screen location of the application was changed to fit in the
    middle of the screen.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -7,6 +7,6 @@ public class Lab3Exercises {
 		myFrame.setLocation(860,400);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
-		//Changes made, view description
+		//Changes made, view description.
 	}
 }
